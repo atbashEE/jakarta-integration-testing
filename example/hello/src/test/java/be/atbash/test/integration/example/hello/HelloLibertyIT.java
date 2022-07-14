07002623e52ca5f3b83bade49b198adf84bfecd9
Rudy De Busscher <rdebusscher@gmail.com>
2022-07-14 21:28:37 +0200

    Examples

---
+++ example/hello/src/test/java/be/atbash/test/integration/example/hello/HelloLibertyIT.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 Rudy De Busscher (https://www.atbash.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.atbash.test.integration.example.hello;
+
+import be.atbash.testing.integration.jupiter.ContainerIntegrationTest;
+import be.atbash.testing.integration.test.OpenLibertyContainerIntegrationTest;
+import org.assertj.core.api.Assertions;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+
+
+@ContainerIntegrationTest
+public class HelloLibertyIT extends OpenLibertyContainerIntegrationTest {
+    // Should be public and not the JUnit 5 preferred scope package.
+
+    @RestClient
+    public HelloService helloService;
+
+    @Test
+    void sayHello() {
+        String value = helloService.sayHello("JUnit");
+        Assertions.assertThat(value).isEqualTo("Hello JUnit");
+    }
+}
